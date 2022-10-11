b272ae6175bf8365fe28d8e15119b117cd5f4273
manoel bertin <manoeltirj@gmail.com>
2022-10-11 09:10:34 -0300

    spec/factories/requerimentos sistema

--- spec/factories/system_requirements.rb
+++ spec/factories/system_requirements.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :system_requirement do
     sequence(:name) { |n| "Basic #{n}" }
-    operational_system { "MyString" }
-    storage { "MyString" }
-    processor { "MyString" }
-    memory { "MyString" }
-    video_board { "MyString" }
+    operational_system { Faker::Computer.os }
+    storage { "500gb" }
+    processor { "AMD Ryzen 7" }
+    memory { "2gb" }
+    video_board { "GeForce XY" }
   end
 end
