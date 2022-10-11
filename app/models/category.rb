de9baeccc7c13a91a344c7f44820ee95bf745254
manoel bertin <manoeltirj@gmail.com>
2022-10-11 08:59:59 -0300

    validando campos requerimento do sistema

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,3 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :operational_system, presence: true
-  validates :storage, presence: true
-  validates :processor, presence: true
-  validates :memory, presence: true
-  validates :video_board, presence: true
 end
