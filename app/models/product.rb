1d6e428d41b65ba8714160122bc9345de0a54c49
manoel bertin <manoeltirj@gmail.com>
2022-10-14 09:20:33 -0300

    validando descrição

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :description, presence: true
 
 end
