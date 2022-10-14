1386341afb4781ba490d5cdba1941e63a0fe1548
manoel bertin <manoeltirj@gmail.com>
2022-10-14 09:21:01 -0300

    validando o preço

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
+  validates :price, presence: true
 
 end
