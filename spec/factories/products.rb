02c8fdf3eeab68c56030931f317685d07618fa0e
manoel bertin <manoeltirj@gmail.com>
2022-10-14 09:29:40 -0300

    gerar preços aleatório num intervalo

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Basic #{n}" }
-    description { "MyText" }
-    price { "9.99" }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Commerce.price(range: 100.0..400.0) }
     productable { nil }
   end
 end
