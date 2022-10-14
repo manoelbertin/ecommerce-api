5537b7daf30346e60434c7295b20d859be9db44d
manoel bertin <manoeltirj@gmail.com>
2022-10-14 08:10:51 -0300

    testes na tabela produto preço > 0

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:price) }
-  it { is_expected.to validate_numericality_of(:price).is_greater_then(0) }
+  it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
 end
