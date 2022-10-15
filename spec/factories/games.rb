5f7362325a8e0c04d1c96736c811176caae58686
manoel bertin <manoeltirj@gmail.com>
2022-10-15 16:49:46 -0300

    sign a mesma forma da anterio

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :game do
-    mode { [:pvp, :pve, :both].sample }
+    mode { %i(pvp pve both).sample }
     release_date { "2022-10-15 15:30:28" }
     developer { "MyString" }
     system_requirement { nil }
