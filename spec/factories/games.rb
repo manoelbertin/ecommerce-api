d0afb9cfe272f285fb2764f5dbef53a1cb6a07f6
manoel bertin <manoeltirj@gmail.com>
2022-10-15 16:47:56 -0300

    no factory estava devolmendo o pvp 1

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :game do
-    mode { 1 }
+    mode { [:pvp, :pve, :both].sample }
     release_date { "2022-10-15 15:30:28" }
     developer { "MyString" }
     system_requirement { nil }
