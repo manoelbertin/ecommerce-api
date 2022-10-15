c826642d7839f1d150bcd9c8c92e822ebfcc924d
manoel bertin <manoeltirj@gmail.com>
2022-10-15 16:40:53 -0300

    valido a presença

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ApplicationRecord
   belongs_to :system_requirement
   has_one :product, as: :productable
   validates :mode, presence: true
+  validates :release_date, presence: true
+
 end
