22317a099a037511b3180f677f260240e2d1b74d
manoel bertin <manoeltirj@gmail.com>
2022-10-15 16:51:42 -0300

    utilizo o Faker para trazer o desenvolvedor

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :game do
     mode { %i(pvp pve both).sample }
     release_date { "2022-10-15 15:30:28" }
-    developer { "MyString" }
+    developer { Faker::Company.name }
     system_requirement { nil }
   end
 end
