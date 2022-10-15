7c2c7822230034cb17ce7decc3413e863701526a
manoel bertin <manoeltirj@gmail.com>
2022-10-15 16:11:25 -0300

    voltando para testar

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,4 @@
 class Game < ApplicationRecord
   belongs_to :system_requirement
   has_one :product, as: :productable
-  validates :mode, presence :true
-  validates :release_date, presence :true
-  validates :developer, presence :true
 end
