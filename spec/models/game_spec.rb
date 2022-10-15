747074293e6e9cec17e2ec33cea08bbc37884450
manoel bertin <manoeltirj@gmail.com>
2022-10-15 15:48:45 -0300

    validação de presença dados do programador game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   it { is_expected.to validate_presence_of(:mode) }
   it { is_expected.to validate_presence_of(:release_date) }
+  it { is_expected.to validate_presence_of(:developer) }
 
 
 end
