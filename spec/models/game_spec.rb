d2364563d46e4e596fff5a6bf46e59a1393bcd53
manoel bertin <manoeltirj@gmail.com>
2022-10-15 15:56:58 -0300

    validando as opçoes de jogo

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Game, type: :model do
   it { is_expected.to validate_presence_of(:release_date) }
   it { is_expected.to validate_presence_of(:developer) }
   it { is_expected.to belong_to :system_requirement }
+  it { is_expected.to define_enum_for(:mode).with_values({ pvp: 1, pve: 2, both: 3}) }
 
 end
