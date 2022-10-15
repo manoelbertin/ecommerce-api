311bb1690c476ed0d61b0bd599a1909bd2c6b5a2
manoel bertin <manoeltirj@gmail.com>
2022-10-15 16:52:44 -0300

    vai criar e associar com meu factory_bot

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     mode { %i(pvp pve both).sample }
     release_date { "2022-10-15 15:30:28" }
     developer { Faker::Company.name }
-    system_requirement { nil }
+    system_requirement
   end
 end
