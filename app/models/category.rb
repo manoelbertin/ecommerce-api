35f6ac3229cc05abf34ad54afe73a8b58020c46a
manoel bertin <manoeltirj@gmail.com>
2022-10-16 15:01:59 -0300

    fiz esse

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :product_categories, dependent: :destroy
-  has_many :products, through: : product_categories
+  has_many :products, through: :product_categories
 end
