18a44ab405220a9d106f77a773635b92cb3a0254
manoel bertin <manoeltirj@gmail.com>
2022-10-17 07:47:08 -0300

    configurando o mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
+  # Don't care if the mailer can't send. 
+  # Aqui nós não vamos EXPLODIR um erro...
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'http//localhost:3000' }
