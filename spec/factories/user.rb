eaa856e69f3e990697a18fefd612b5949cb8a50b
manoel bertin <manoeltirj@gmail.com>
2022-10-25 18:08:00 -0300

    preparei o model user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name
-    email 
-    password 
-    password_confirmation 
-    profile
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { "123456" }
+    password_confirmation { "123456" }
+    profile { %i(admin client).sample }
   end
 end
