bd893fc8a3138a62e6da663113c23f2ba56f40e0
manoel bertin <manoeltirj@gmail.com>
2022-10-29 21:39:00 -0300

    operadores E && e OU ||

--- app/validators/future_date_validator.rb
+++ app/validators/future_date_validator.rb
@@ -1,2 +1,6 @@
 class FutureDateValidator < ActiveModel::EachValidator 
+  def validate_each(record, attribute, value)
+    if value.present? && value <= Time.zone.now 
+      message = options[:message] || :future_date
+    end
 end
