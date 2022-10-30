6418119750e7a8ec443a6767c18e04b123e6af0d
manoel bertin <manoeltirj@gmail.com>
2022-10-29 22:23:24 -0300

    só havia colocado 1.day

--- spec/validators/future_date_validator_spec.rb
+++ spec/validators/future_date_validator_spec.rb
@@ -36,7 +36,7 @@ describe FutureDateValidator do
   end
 
   context "when date is greater than current date" do
-    before { subject.date = 1.day }
+    before { subject.date = Time.zone.now + 1.day }
 
     it "should be valid" do 
       expect(subject.valid?).to be_truthy 
