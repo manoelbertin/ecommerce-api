50957355e95d2151a6c234221bc991d14a26062f
manoel bertin <manoeltirj@gmail.com>
2022-11-07 20:04:56 -0300

    espera -se que o retorno json tenha categorias

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe "Admin::V1::Categories", type: :request do
   context "GET /categories" do
     let(:url) { "/admin/v1/categories" }
     let!(:categories) { create_list(:category, 5) }
+
+
+    it "returns all Categories" do
+      get url, headers: auth_header(user)
+      expect(body_json['categories']).to contain_exactly categories
+    end
   end
   
 end
