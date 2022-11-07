595e39633ccb7926e2bb149772c5d70c4f1a7629
manoel bertin <manoeltirj@gmail.com>
2022-11-06 22:12:37 -0300

    criar um usuário para fazer as chamadas auth

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::V1::Categories", type: :request do
+  let(:user) { create(:user) }
 
+  context "GET /categories" do
+    let(:url) { "/admin/v1/categories"  }
+    
+  end
+  
 end
