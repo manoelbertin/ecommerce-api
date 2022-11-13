16c6bb46a9ea8d065b76ab7319f3108d0b830bfc
manoel bertin <manoeltirj@gmail.com>
2022-11-13 10:05:40 -0300

    Criado um novo contexto

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -19,4 +19,20 @@ RSpec.describe "Admin::V1::Categories", type: :request do
     end
   end
   
+  context "POST /categories" do
+    let(:url) { "/admin/v1/categories" }
+    let!(:categories) { create_list(:category, 5) }
+
+
+    it "returns all Categories" do
+      get url, headers: auth_header(user)
+      expect(body_json['categories']).to contain_exactly *categories.as_json(only: %i(id name))
+    end
+
+    it "returns success status" do
+      get url, headers: auth_header(user)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
 end
