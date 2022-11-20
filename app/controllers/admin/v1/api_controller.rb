0bf94121e61751636f69c5a0499d63b6ebbe8dc4
manoel bertin <manoeltirj@gmail.com>
2022-11-20 20:35:01 -0300

    melhoria para mensagens de erro

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,5 +1,10 @@
 module Admin::V1
   class ApiController < ApplicationController
     include Authenticable
+
+    def render_error (message: nil, fields: nil, status: :unprocessable_entity)
+      
+    end
+
   end
 end
