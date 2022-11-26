4ef58d9ad771e939751f62c9cb51614c5ed15dd5
manoel bertin <manoeltirj@gmail.com>
2022-11-26 08:46:49 -0300

    estou atualizando a action update

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -70,12 +70,12 @@ RSpec.describe "Admin::V1::Categories", type: :request do
   end
 
   context "PATCH /categories/:id" do 
-    let (category) { create(:category) }
+    let (:category) { create(:category) }
     let(:url) { "/admin/v1/categories/#{categorie.id}" }
 
       context "with valid params" do 
         let(:new_name) { 'My new Category' }
-        let(:category_params) { category: {name: new_name}.to_json }
+        let(:category_params) { {category: {name: new_name}}.to_json }
 
         it 'updates Category' do 
           patch url, headers: auth_header(user), params: category_params
@@ -106,6 +106,16 @@ RSpec.describe "Admin::V1::Categories", type: :request do
           patch url, headers: auth_header(user), params: category_invalid_params
           category.reload
           expect(category.name).to eq old_name
+        end
+
+        it 'returns unprocessable_entity status' do 
+          patch url, headers: auth_header(user), params: category_invalid_params
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it "return error messages" do 
+          patch url, headers: auth_header(user), params: category_invalid_params
+          expect(body_json['errors']['fields']).to have_key('name')
         end
       end
   end
